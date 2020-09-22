a95a0c15fd0caf2b0a55be656af4df3cb049363d
juunone <juunone@gmail.com>
2020-09-22 22:39:55 +0900

    feat: Add e2e test and storybook applitools snapshot test

--- src/components/__test__/List.test.js
+++ src/components/__test__/List.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
+import { deleteOne } from "../../utils";
 import { List } from "../List";
 
 describe("List", () => {
   it("should render as long as the data", () => {
-    render(<List data={["첫번째", "두번째"]} deleteTodoList={jest.fn()} />);
+    render(<List data={["첫번째", "두번째"]} deleteTodoList={deleteOne} />);
     expect(screen.getByText(/첫번째/i)).toBeInTheDocument();
   });
 });
